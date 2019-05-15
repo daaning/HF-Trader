719abc78e38aaab656f32add86ec2e39c1f2dcd9
daan.lockhorst <dlockhorst@mpiwg-berlin.mpg.de>
2019-05-15 09:34:11 +0200

    new version

--- Settings.py
+++ Settings.py
@@ -1,4 +1,5 @@
 
+import json
 """
 these are the settings you have to set before trading
 """
@@ -6,37 +7,35 @@ these are the settings you have to set before trading
 tradecurrency = "BTC"
 
 # markets where to trade in
-tradewith = ["ETH",
-             "XVG",
-             "XMR"]
+tradewith = ["BNB",
+             "USDT"]
 
 # stake amount in tradecurrency
 stakeamount = 0.001
 
-# maximum amount of trades the bot will acquire 
+# maximum amount of trades the bot will acquire
 maxstakes = 5
 
 # if your strategy doesnt sell without profit set. minimum Return of investment
 # 1.00 is 100 procent return on original investment
-minROI = 1.01 # minimum ROI in % * 100 on selling with stoploss
+minROI = 1.01  # minimum ROI in % * 100 on selling with stoploss
 
 """
 these are the settings you have to set in the Config.json file they are to be found 
 at twitter and binance
 """
 
-import json
 config = json.load(open('Config.json'))
 
-#the keys to your twitter API 
+# the keys to your twitter API
 twitter_key = config['twitter_key']
 twitter_secret = config['twitter_secret']
 twitter_tokenkey = config['twitter_tokenkey']
 twitter_tokensecret = config['twitter_tokensecret']
 
 # the keys to your binance API
-key= config['key']
-secret= config['secret']
+key = config['key']
+secret = config['secret']
 
 
 """
